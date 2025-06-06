3919fba4def73eddca782bc3516bec7756462a0c
Mathias-Kabango3 <m.kabango@alustudent.com>
2025-06-06 11:57:23 +0200

    feat: add Reminder model with message, remind_at(time) and method

--- remindme/api/models.py
+++ remindme/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Reminder(models.Model):
+    REMINDER_METHODS = [
+        ('sms', 'SMS'),
+        ('email', 'Email'),
+    ]
+
+    message = models.TextField()
+    remind_at = models.DateTimeField()
+    method = models.CharField(max_length=10, choices=REMINDER_METHODS)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.method.upper()} reminder at {self.remind_at}"
